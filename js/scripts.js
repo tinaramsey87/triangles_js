7ec1e2e4f3562c5c5e0685cc38c87b063051153f
Tina Ramsey <tina.ramsey87@gmail.com>
2015-02-13 10:46:04 -0800

    add website functionality

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,43 @@ var triangle = function(sideA, sideB, sideC) {
   }
 };
 
-$(document.ready(function(){
+$(document).ready(function() {
+  $("form#triangle-tester").submit(function(event) {
+    var sideA = parseInt($("input#sideA").val());
+    var sideB = parseInt($("input#sideB").val());
+    var sideC = parseInt($("input#sideC").val());
+    var result = triangle(sideA, sideB, sideC);
 
+    $(".sideA").text(sideA);
+    $(".sideB").text(sideB);
+    $(".sideC").text(sideC);
+
+
+    $("#result").show();
+
+    if (result === "eqilateral") {
+      $("#tri-yes").show();
+      $("#tri-no").hide();
+      $(".tri-type").text(result)
+    }
+
+    if (result === "isosceles") {
+      $("#tri-yes").show();
+      $("#tri-no").hide();
+      $(".tri-type").text(result)
+    }
+
+    if (result === "scalene") {
+      $("#tri-yes").show();
+      $("#tri-no").hide();
+      $(".tri-type").text(result)
+    }
+
+    if (!result) {
+      $("#tri-no").show();
+      $("#tri-yes").hide();
+    }
+
+    event.preventDefault();
+  });
 });
